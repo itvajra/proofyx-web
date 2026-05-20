92d70e80cf40279668449ac4996bcd01aad1e94e
itvajra <vajra360company@gmail.com>
2026-05-20 18:28:13 -0500

    update to www.proofyx.com for image preview fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://proofyx.com',
+  site: 'https://www.proofyx.com',
   build: {
     inlineStylesheets: 'always'
   },
